df7cb45c653811f11f19e1478e78a7fa0b19cc4c
ValenBentos <88683261+ValenBentos@users.noreply.github.com>
2023-06-14 12:49:17 -0300

    examen1_Barcia-Bentos

---
+++ examen1_files_Barcia-Bentos/src/main/java/preparacionprueba/Datos.java
@@ -0,0 +1,37 @@
+package preparacionprueba;
+
+import java.util.logging.Logger;
+
+/**
+ *
+ * ACA UTILIZAR COLECCIONES PARA ALMACENAR LOS DATOS Y PODER TRABAJAR CON ELLOS
+ * ADEMAS PEDIR LOS DATOS UEVOS A LOS USUARIO Y CALCULAR EL MES DE MAYOR
+ * VICTORIAS
+ *
+ * @author
+ */
+public class Datos {
+
+    private static final Logger LOG = Logger.getLogger(Datos.class.getName());
+
+    public Datos() {
+    }
+
+    public void cargarNuevo() {
+
+    }
+
+    /**
+     *
+     */
+    public void almacenar() {
+
+    }
+
+    /**
+     *
+     */
+    public void calcularVictorias() {
+
+    }
+}
---
+++ examen1_files_Barcia-Bentos/src/main/java/preparacionprueba/Main.java
@@ -0,0 +1,27 @@
+package preparacionprueba;
+
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author 
+ */
+public class Main {
+    
+    private static final Logger LOG = Logger.getLogger(Main.class.getName());
+    
+    /**
+     * SOLO PREPARAR EL ARCHIVO (CON ORIENCION A OBJETOS) Y LUEGO LLAMAR AL FUNCIONAMIENTO DEL PROGRAMA
+     * @param args 
+     */
+    public static void main(String[] args) {
+        
+        
+        
+        
+        
+    }
+    
+    
+    
+}
